d2546e2bd74450b60a5315e358ce440fbaba0497
Ayush Agarwal <aa2503452@gmail.com>
2023-07-18 01:44:11 +0530

    adding a change

--- src/App.js
+++ src/App.js
@@ -12,10 +12,8 @@ export default function App() {
   const [task, setTask] = useState("");
 
   function handleAddTask(task) {
-    if (task) {
-      setTasksArr((prevArr) => [...prevArr, { task, id: uuid() }]);
-      setTask("");
-    }
+    setTasksArr((prevArr) => [...prevArr, { task, id: uuid() }]);
+    setTask("");
   }
 
   function handleDelete(id) {
